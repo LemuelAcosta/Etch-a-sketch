84f48a87d9d7fe44e5e54400aecc817bca0c909d
Lemuel <63744340+LemuelAcosta@users.noreply.github.com>
2025-05-07 14:45:44 -0400

    Finish product

--- javascript.js
+++ javascript.js
@@ -1,22 +1,41 @@
 const CONTAINER = document.querySelector("#Container");
+const RESET = document.querySelector("#Reset")
+const INPUT = document.querySelector("#inputText")
+const SEND_BUTTON = document.querySelector("#sendButton") 
 
 let boxes = 256;
 drawBoxes(boxes);
-// let grid = document.createElement("div")
 
 function drawBoxes(boxes) {
     for (i = 1; i<=boxes; i++) {
         const GRID = document.createElement("div");
         GRID.classList.add("box");
-        GRID.textContent = i
         CONTAINER.appendChild(GRID);
     }
 }
 
+RESET.addEventListener("click", resetGrid);
 
 CONTAINER.addEventListener('mouseenter', (event) => {
     if (event.target.classList.contains('box')) {
       event.target.classList.add('highlight');
     }
   }, true); 
+
+SEND_BUTTON.addEventListener("click", (e) => {
+    boxes = INPUT.value;
+    if(boxes > 128) {
+        boxes = 128
+    }
+    resetGrid();
+    drawBoxes(boxes)
+    console.log(boxes)
+})
+
+function resetGrid() {
+    const BOXES = document.querySelectorAll(".box");
+    BOXES.forEach(box => {
+        box.remove();
+    });
+}
 
